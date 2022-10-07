e56b3164b81104e21ae9e26273b4e500bef165bd
RashedEmon <rashedulb13@gmail.com>
2022-10-07 18:30:38 +0600

    all working fine

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index,login_view
+from .views import index,login_view,logout_view
 urlpatterns = [
     path('', index),
     path('login/', login_view),
+     path('logout/', logout_view),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,5 @@
+from sqlite3 import Date
+from venv import create
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods
@@ -5,17 +7,26 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from .forms import LoginForm
 
+
+from .models import Message
 # Create your views here.
-@require_http_methods(["GET"])
+@require_http_methods(["GET","POST"])
 def index(request):
     if not request.user.is_authenticated:
         login_form=LoginForm().as_table()
         return render(request,"login.htm",{"form": login_form})
     
-    return render(request,"chat.htm")
-
-
-
+    if request.method == "GET":
+        msg= Message.objects.all().order_by("created")
+        # for m in msg:
+        #     m.msg_from
+        return render(request,"chat.htm",{"msg": msg})
+    if request.method == "POST":
+        msg=request.POST["message"]
+        print(msg)
+        new_msg=Message(message=msg,msg_from=request.user)
+        new_msg.save()
+        return redirect("/")
 
 @require_http_methods(["GET","POST"])
 def login_view(request):
@@ -43,7 +54,13 @@ def login_view(request):
 
 
 
-def logout(request):
+def logout_view(request):
     logout(request)
 
-    return render(request,"/login")
+    return redirect("/login")
+
+
+
+
+
+
